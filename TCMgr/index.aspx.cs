12a86b6992df66adbcab3168454f0fbf0a10bee5
JohnToCoder <zh.wolf5@gmail.com>
2014-03-06 01:31:12 +0800

    0306

--- TCMgr/index.aspx.cs
+++ TCMgr/index.aspx.cs
@@ -9,37 +9,9 @@ namespace TCMgr
 {
     public partial class index : System.Web.UI.Page
     {
-        public string strUserName = "";
-        public string strUserType = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-            if (!Page.IsPostBack) {
-               strUserName = Session["UserName"].ToString()+" 欢迎您！";
-               strUserType = Session["UserType"].ToString();
-               InitLeftMenu();
-            }            
-        }
 
-        private void InitLeftMenu()
-        {
-            string strUserType = Session["UserType"].ToString();
-            if (strUserType == "0") {
-                AdminMenu.Visible = true;
-                MgrMenu.Visible = false;
-                UserMenu.Visible = false;
-            }
-            else if (strUserType == "1")
-            {
-                AdminMenu.Visible = false;
-                MgrMenu.Visible = true;
-                UserMenu.Visible = false;
-            }
-            else {
-                AdminMenu.Visible = false;
-                MgrMenu.Visible = false;
-                UserMenu.Visible = true;
-            }
         }
     }
 }
